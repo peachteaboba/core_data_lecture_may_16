70f022aebd1293bbdd4d6946bee8656e0443ccd9
Andy Feng <anndyfeng1@gmail.com>
2017-05-16 10:16:34 -0700

    Initial Commit

---
+++ core_data_bucket_list_may_16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  core_data_bucket_list_may_16
+//
+//  Created by Andy Feng on 5/16/17.
+//  Copyright © 2017 Andy Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
